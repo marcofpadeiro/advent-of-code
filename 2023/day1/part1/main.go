05866fe1e822922b646264b3d6cb06c9306ad422
marcofpadeiro <2231953@my.ipleiria.pt>
2023-12-01 14:51:00 +0000

    Day 1 Part 1

---
+++ 2023/day1/part1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+	"unicode"
+)
+
+func main() {
+	start := time.Now()
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+
+	for scanner.Scan() {
+		firstInt := 0
+		lastInt := 0
+
+		line := []rune(scanner.Text())
+
+		for i := 0; i < len(line); i++ {
+			if unicode.IsDigit(line[i]) {
+				firstInt = int(line[i]) - '0'
+				break
+			}
+		}
+		for i := len(line) - 1; i >= 0; i-- {
+			if unicode.IsDigit(line[i]) {
+				lastInt = int(line[i]) - '0'
+				break
+			}
+		}
+
+		total += int(firstInt)*10 + int(lastInt)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println("Result:", total)
+
+	fmt.Println("Took", time.Since(start))
+}
