0f5522a1de8cf7ebe5ee147738406d6751466e76
marcofpadeiro <2231953@my.ipleiria.pt>
2023-12-06 21:29:39 +0000

    Day 5 Part 1 Recursion

--- 2023/day5/part1.go
+++ 2023/day5/part1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +14,52 @@ type Instruction struct {
 	source_start int
 	source_end   int
 	dest_start   int
+}
+
+func getSeeds(line string) []int {
+	seeds_str := strings.Split(line, ":")
+	seeds_arr := strings.Fields(seeds_str[1])
+
+	var seeds []int
+	for _, seed := range seeds_arr {
+		seed_int, _ := strconv.Atoi(seed)
+		seeds = append(seeds, seed_int)
+	}
+	return seeds
+}
+
+func getBlocks(lines []string) [7][]Instruction {
+	var blocks [7][]Instruction
+	currBlock := 0
+	for i := 0; i < len(lines); i++ {
+		if lines[i] == "" {
+			i++
+			currBlock++
+			continue
+		}
+
+		numbers_str := strings.Fields(lines[i])
+		var numbers [3]int
+		for n_index, n := range numbers_str {
+			temp_int, _ := strconv.Atoi(n)
+			numbers[n_index] = temp_int
+		}
+
+		blocks[currBlock] = append(blocks[currBlock], Instruction{numbers[1], numbers[1] + numbers[2], numbers[0]})
+	}
+	return blocks
+}
+
+func walk(seed int, instructions []Instruction, currInstruction int) int {
+	if currInstruction == len(instructions) {
+		return seed
+	}
+	if seed >= instructions[currInstruction].source_start && seed < instructions[currInstruction].source_end {
+		return seed - instructions[currInstruction].source_start + instructions[currInstruction].dest_start
+	}
+	currInstruction++
+
+	return walk(seed, instructions, currInstruction)
 }
 
 func main() {
@@ -27,63 +74,31 @@ func main() {
 
 	start := time.Now()
 
-	var seeds []int
 	var lines []string
-	var instructions []Instruction
 
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-
-	seeds_str := strings.Split(lines[0], ":")
-	seeds_arr := strings.Fields(seeds_str[1])
-
-	for _, seed := range seeds_arr {
-		seed_int, _ := strconv.Atoi(seed)
-		seeds = append(seeds, seed_int)
-	}
-
-	for i := 3; i < len(lines); i++ {
-		if lines[i] == "" || i == len(lines)-1 {
-			if len(instructions) > 0 {
-				for index, seed := range seeds {
-					for _, instruction := range instructions {
-						if seed >= instruction.source_start && seed < instruction.source_end {
-							seeds[index] = seed - instruction.source_start + instruction.dest_start
-						}
-					}
-				}
-			}
-
-			i++
-			instructions = nil
-			continue
-		}
-
-		numbers_str := strings.Fields(lines[i])
-		var numbers [3]int
-		for n_index, n := range numbers_str {
-			temp_int, _ := strconv.Atoi(n)
-			numbers[n_index] = temp_int
-		}
-
-		instructions = append(instructions, Instruction{numbers[1], numbers[1] + numbers[2], numbers[0]})
-	}
-
-	// insert seed value and go through maps
-	lowest := seeds[0]
-	for i := 1; i < len(seeds); i++ {
-		if seeds[i] < lowest {
-			lowest = seeds[i]
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Print(err)
-	}
-
 	time := time.Since(start)
+
+	seeds := getSeeds(lines[0])
+
+	blocks := getBlocks(lines[3:])
+
+	lowest := math.MaxInt64
+
+	for _, seed := range seeds {
+		endVal := seed
+		for _, block := range blocks {
+			endVal = walk(endVal, block, 0)
+		}
+		if endVal < lowest {
+			lowest = endVal
+		}
+	}
+
 	fmt.Println("Result:", lowest)
 
 	fmt.Println("Took", time)
+
 }
